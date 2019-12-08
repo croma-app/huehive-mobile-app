ef6273d6a0e5b25261a5c523f679fe5d8fe035d5
kamal <kkjoshi.it@gmail.com>
2019-12-08 14:36:12 +0530

    details in the color details view

--- components/ColorDetails.js
+++ components/ColorDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Button,TouchableNativeFeedback } from 'react-native';
+import Color from 'pigment/full';
 
 export function ColorDetail(props) {
   const backgroundColor = {
@@ -8,15 +9,55 @@ export function ColorDetail(props) {
   const styles = StyleSheet.create({
     backgroundColor: {
       backgroundColor: props.color, height: 200, alignSelf: 'stretch'
-    }
+    },
+    info: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      padding: 16,
+    },
   });
-  return (
+  const color = new Color(props.color);
+  let items = 
+    [
+      { key: 'HEX', value: props.color },
+      { key: 'RGB', value: color.torgb() },
+      { key: 'HSL', value: color.tohsl() },
+      { key: 'HSV', value: color.tohsv() },
+      { key: 'HWB', value: color.tohwb() },
+      { key: 'CMYK', value: color.tocmyk() },
+      { key: 'CIELAB', value: color.tolab() },
+      { key: 'Luminance', value: (color.luminance() * 100).toFixed(2) + '%' },
+      { key: 'Darkness', value: (color.darkness() * 100).toFixed(2) + '%' },
+    ];
+  this._copyToClipboard = function(value) {
     
-  
+  }
+  return (
     <View style={{flex: 1, flexDirection: 'column'}}>
       <View style={[styles.backgroundColor]} ></View>
-      <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} >{props.color}</Text>
-
+      {/* <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} >{props.color}</Text> */}
+      <View >
+        {items.map(item => (
+          <TouchableNativeFeedback
+           key={item.key}
+           onPress={() => this._copyToClipboard(item.value)}
+          >  
+            <View style={styles.info}>
+              
+              <Text>{item.key}</Text>
+              <Text>:</Text
+              ><Text>{item.value}</Text>
+              <Button
+                onPress={this.writeToClipboard}
+                title="Read from Clipboard"
+              />
+              
+            </View>
+          </TouchableNativeFeedback>
+        ))}
+        
+      </View>
     </View>
   );
 }
