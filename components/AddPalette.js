7323297c712007d8e8d3326a0cd733234ba3d338
kamal <kkjoshi.it@gmail.com>
2019-12-28 10:05:00 +0530

    bug fix in color convetion from int to hex

--- components/AddPalette.js
+++ components/AddPalette.js
@@ -11,56 +11,11 @@ import {
   View,
   Alert,
   StyleSheet,
+  Image,
 } from "react-native";
 
 export class AddPalette extends React.Component {
-  componentDidMount() {
-    this.getPermissionAsync();
-    //console.log('hi');
-  }
-
-  getPermissionAsync = async () => {
-    if (Constants.platform.ios) {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-      if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!');
-      }
-    }
-  }
-
-  _pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      quality: 1, 
-      base64: true,
-    });
-
-    //console.log("image" + result, "base64", result.base64);
-   // Jimp.read(new Buffer(result.base64, 'base64'))
-   console.log("Result: " + JSON.stringify(result));
-   //console.log("base64: " + result.base64.length);
-   if (result.base64 !== undefined) {
-    Jimp.read(new Buffer(result.base64, "base64"))
-    .then(image => {
-      this.props.navigation.navigate("ColorList", {colors: this._getProminentColors(image)}); 
-    })
-    .catch(err => {
-      console.log("Error: " + JSON.stringify(err)); // TODO: add toast here 
-    // Handle an exception.
-    });
-   } else {
-    Jimp.read(result.uri)
-      .then(image => {
-        this.props.navigation.navigate("ColorList", {colors: this._getProminentColors(image)}); 
-      })
-      .catch(err => {
-        console.log("Error: " + JSON.stringify(err)); // TODO: add toast here 
-      // Handle an exception.
-      });
-  }
-
-    
-  };
+  
   render() {
     return (
       <View>
@@ -88,9 +43,57 @@ export class AddPalette extends React.Component {
         >
            UNLOCK PRO
         </CromaButton>
+        <Image
+          style={{width: 50, height: 50}}
+          source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
+        />
       </View>
     );
   }
+
+  componentDidMount() {
+    this.getPermissionAsync();
+    //console.log('hi');
+  }
+
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      quality: 1, 
+      base64: true,
+    });
+   // console.log("Result: " + JSON.stringify(result));
+   if (result.base64 !== undefined) {
+    Jimp.read(new Buffer(result.base64, "base64"))
+    .then(image => {
+      this.props.navigation.navigate("ColorList", {colors: this._getProminentColors(image)}); 
+    })
+    .catch(err => {
+      console.log("Error: " + JSON.stringify(err)); // TODO: add toast here 
+    // Handle an exception.
+    });
+   } else {
+    Jimp.read(result.uri)
+      .then(image => {
+        this.props.navigation.navigate("ColorList", {colors: this._getProminentColors(image)}); 
+      })
+      .catch(err => {
+        console.log("Error: " + JSON.stringify(err)); // TODO: add toast here 
+      // Handle an exception.
+      });
+  }
+
+    
+  };
 
 
   _getProminentColors(image) {
@@ -103,15 +106,10 @@ export class AddPalette extends React.Component {
     Jimp.RESIZE_BEZIER;
     These does not work with first params.
     */
-    try {
-    image.resize(Jimp.AUTO, 50);
-    } catch(e) {
-      console.log("error------", e);
-    }
-    console.log("resized");
+    image.resize(Jimp.AUTO, 100);
     let data = this._prepareDataForKmeans(image);
     let time = Date.now()
-    let ans = kmeans(data, 24, { initialization: 'mostDistant', maxIterations: 15 });
+    let ans = kmeans(data, 24, { initialization: 'random', maxIterations: 20 });
     console.log(JSON.stringify(ans) + "," + (Date.now() - time) + " ms");
     ans.centroids = ans.centroids.sort((c1, c2) => c2.size - c1.size);
 
@@ -120,20 +118,22 @@ export class AddPalette extends React.Component {
   }
 
   _xyzToHex(xyz) {
-    let color = new Color("xyz(" + xyz[0] + ", " + xyz[1] + ", " + xyz[2] +  ")");
+    let color = new Color("lab(" + xyz[0] + ", " + xyz[1] + ", " + xyz[2] +  ")");
     console.log("color===========" + color.tohex());
     return color.tohex();
   }
 
   _prepareDataForKmeans(image) {
     let data = [];
+    
     console.log("image============", image.bitmap.width, image.bitmap.height);
     for (let i = 0; i < image.bitmap.width; i++) {
       for (let j = 0; j < image.bitmap.height; j++) {
         let intColor = image.getPixelColor(i, j);
-        let hexColor = this._toRgbaColor(intColor);
-        let color = new Color(hexColor);
-        let xyz = color.toxyz();
+        let hex = this._toHexColor(intColor);
+        console.log("hex:", hex);
+        let color = new Color(hex);
+        let xyz = color.tolab();
         // format: "xyz(19.78527130484015, 8.600439447528947, 95.19796416837329)" to double array of xyz
         xyz = xyz.substr(4, xyz.length - 5).split(", ").map(v => parseFloat(v))
         data.push(xyz);
@@ -143,13 +143,10 @@ export class AddPalette extends React.Component {
     return data;
   }
 
-  _toRgbaColor(num) {
-    num >>>= 0;
-    var b = num & 0xFF,
-        g = (num & 0xFF00) >>> 8,
-        r = (num & 0xFF0000) >>> 16,
-        a = ( (num & 0xFF000000) >>> 24 ) / 255 ;
-    return "rgba(" + [r, g, b, a].join(",") + ")";
+  _toHexColor(intColor) {
+    let rgba = Jimp.intToRGBA(intColor); // TODO: Need to optimize this once everything else starts working.
+    let color = new Color("rgb(" + rgba.r + ", " + rgba.g + ", " + rgba.b + ")");
+    return color.tohex();
   }
 }
 
