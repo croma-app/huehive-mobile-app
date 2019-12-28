a185d09f30eec192a1e6030b89fd2f43f9f6bc6b
kamal <kkjoshi.it@gmail.com>
2019-12-29 01:51:23 +0530

    final filtering from 24 colors to 6 colors * https://github.com/kamalkishor1991/croma/blob/4cc7a0fada6781b30fb681daf9028a56106b0079/src/main/java/org/numixproject/colorextractor/image/KMeansColorPicker.java#L21

--- libs/ColorPicker.js
+++ libs/ColorPicker.js
@@ -18,16 +18,38 @@ export default class ColorPicker {
     let ans = kmeans(data, 24, { initialization: 'random', maxIterations: 20 });
     console.log(JSON.stringify(ans) + "," + (Date.now() - time) + " ms");
     ans.centroids = ans.centroids.sort((c1, c2) => c2.size - c1.size);
-
     console.log(ans.centroids);
-    //let kmeansColors  = ans.centroids.map(centroid => {return new Color(this._labToHex(centroid.centroid))});
-    //kmeansColors.sort((c1, c2) => parseFloat(c1.tohsv().substr(4).split(", ")) < parseFloat(c2.tohsv().substr(4).split(", ")));
-    return ans.centroids.map(centroid => {return {color: this._labToHex(centroid.centroid)}});
+    let kmeansColors  = ans.centroids.map(centroid => {return new Color(this._labToHex(centroid.centroid))});
+    return this._getFinalColors(kmeansColors).map(c => {return {color: c.tohex()}});
+  }
+  // original implementation in java: https://github.com/kamalkishor1991/croma/blob/master/src/main/java/org/numixproject/colorextractor/image/KMeansColorPicker.java
+  static _getFinalColors(kmeansColors) {
+    console.log("----------------------", kmeansColors);
+    kmeansColors.sort((c1, c2) => this._toArray(c1.tohsv())[0] < this._toArray(c2.tohsv())[0]);
+    let filteredColors = [];
+    for (let i = 0;i < kmeansColors.length; i += 4) {
+      let colorList = [];
+      for (let j = 0; j < 4; j++) {
+        colorList.push(kmeansColors[i + j]);
+      }
+      colorList.sort((c1, c2) => this._toArray(c1.tohsv())[1] < this._toArray(c2.tohsv())[1]);
+      filteredColors.push(colorList[colorList.length - 1]);
+      filteredColors.push(colorList[colorList.length - 2]);
+    }
+    console.log("filtered colors:" + filteredColors);
+    let finalColors = [];
+    for (let i = 0;i < filteredColors.length; i += 2) {
+      if (this._toArray(filteredColors[i].tohsv())[2] > this._toArray(filteredColors[i + 1].tohsv())[2]) {
+        finalColors.push(filteredColors[i]);
+      } else {
+        finalColors.push(filteredColors[i + 1]);
+      }
+    }
+    return finalColors;
   }
 
-  static _labToHex(xyz) {
-    let color = new Color("lab(" + xyz[0] + ", " + xyz[1] + ", " + xyz[2] +  ")");
-    console.log("color===========" + color.tohex());
+  static _labToHex(lab) {
+    let color = new Color("lab(" + lab[0] + ", " + lab[1] + ", " + lab[2] +  ")");
     return color.tohex();
   }
 
@@ -55,5 +77,11 @@ export default class ColorPicker {
     let rgba = Jimp.intToRGBA(intColor); // TODO: Need to optimize this once everything else starts working.
     let color = new Color("rgb(" + rgba.r + ", " + rgba.g + ", " + rgba.b + ")");
     return color.tohex();
+  }
+
+  static _toArray(color) {
+    let index = color.indexOf("(");
+    color = color.substr(index + 1, color.length - index);
+    return color.split(", ").map(c => parseFloat(c));
   }
 }
