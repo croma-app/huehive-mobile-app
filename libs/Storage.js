33ebb66ca78844faa5838618a2b3481603097fd3
bhuwan <bhuwanchandra.it@gmail.com>
2020-01-18 12:47:42 +0530

    get all palettes added

--- libs/Storage.js
+++ libs/Storage.js
@@ -1,6 +1,17 @@
 import { AsyncStorage } from "react-native";
 const ALL_PALETTES = "ALL_PALETTES";
 export default class Storage {
+
+  static getAllPalettes = async () => {
+    let palettes = await AsyncStorage.getItem(ALL_PALETTES);
+    console.log("Palettes returned from storage: ", palettes);
+    if (palettes) {
+      return JSON.parse(palettes);
+    } else {
+      return {};
+    }
+  };
+  
   static saveAllPalette = async allPalette => {
     await AsyncStorage.setItem(ALL_PALETTES, JSON.stringify(allPalette))
   }
