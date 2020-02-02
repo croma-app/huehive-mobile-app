fe93da4196fe9788dfe553fc7c6f554c17978de5
bhuwan <bhuwanchandra.it@gmail.com>
2020-02-02 10:04:33 +0530

    Added animation in card component

--- components/Card.js
+++ components/Card.js
@@ -2,12 +2,13 @@ import * as React from "react";
 import { StyleSheet, Animated } from "react-native";
 import Colors from "../constants/Colors";
 import Touchable from "react-native-platform-touchable";
+import * as Animatable from 'react-native-animatable';
 
 export default class Card extends React.Component {
   render() {
     return (
       <Touchable onPress={this.props.onPress} style={[styles.inner]}>
-        <Animated.View {...this.props}>{this.props.children}</Animated.View>
+        <Animatable.View animation="zoomIn" iterationCount={1} {...this.props}>{this.props.children}</Animatable.View>
       </Touchable>
     );
   }
