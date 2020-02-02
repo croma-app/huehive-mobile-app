b5a49265ab6b1bb876acf6e9a15b6c262bfe7305
kamal <kkjoshi.it@gmail.com>
2020-02-02 08:56:28 +0530

    workaround to fix touch on scroll issue for mobile browser issue #17

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,23 @@
 import * as React from "react";
-import { StyleSheet, Animated } from "react-native";
+import { StyleSheet, Animated, Platform } from "react-native";
 import Colors from "../constants/Colors";
 import Touchable from "react-native-platform-touchable";
 
 export default class Card extends React.Component {
   render() {
     return (
-      <Touchable onPress={this.props.onPress} style={[styles.inner]}>
+      <Touchable
+        {...(Platform.OS === "web"
+          ? {
+              // When scrolling the document body, the touchables might be triggered
+              // see  https://github.com/necolas/react-native-web/issues/1219
+              onClick: this.props.onPress
+            }
+          : {
+              onPress: this.props.onPress
+            })}
+        style={[styles.inner]}
+      >
         <Animated.View {...this.props}>{this.props.children}</Animated.View>
       </Touchable>
     );
