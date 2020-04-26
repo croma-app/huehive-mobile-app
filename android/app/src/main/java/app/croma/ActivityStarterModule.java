3f5133b9aa5d512247b4eeb81203216f25c0677f
Kamal Kishor Joshi <kkjoshi.it@gmail.com>
2020-04-26 13:28:58 +0530

    Pick colors from camera (#59)
    * New activity to experiment with camera for color picking
    
    * Working text recognition poc with a toast
    
    * add basic camera2 api activity to take picture
    
    * clean up
    
    * basic working color picker moved from old app
    
    * calling android activity from react home screen
    
    * working startActivityWithResult
    
    * end to end working camera color picker
    
    * Add action button into the item
    
    * version increase and remove launcher for color picker

---
+++ android/app/src/main/java/app/croma/ActivityStarterModule.java
@@ -0,0 +1,70 @@
+package app.croma;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ActivityStarterModule extends ReactContextBaseJavaModule implements ActivityEventListener {
+
+    private static final int PICK_COLORS = 1;
+    private final ReactApplicationContext reactContext;
+    private Callback callback;
+    public ActivityStarterModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        this.reactContext.addActivityEventListener(this);
+    }
+
+    @Override
+    public String getName() {
+        return "CromaModule";
+    }
+
+    @ReactMethod
+    public void navigateToColorPicker(Callback callback) {
+        this.callback = callback;
+        ReactApplicationContext context = getReactApplicationContext();
+        Intent intent = new Intent(context, ColorPickerActivity.class);
+        context.startActivityForResult(intent, PICK_COLORS, new Bundle());
+    }
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        if (data != null && requestCode == PICK_COLORS) {
+            try {
+                this.callback.invoke(mapToJsonString(data.getIntegerArrayListExtra("colors")));
+            } catch (JSONException e) {
+                throw new IllegalStateException(e);
+            }
+        }
+    }
+
+    private String mapToJsonString(List<Integer> colors) throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        List<JSONObject> colorsObjs = new ArrayList<>();
+        for (int color : colors) {
+            colorsObjs.add(new JSONObject().put("color", String.format("#%06X", (0xFFFFFF & color))));
+        }
+        jsonObject.put("colors", new JSONArray(colorsObjs));
+        return jsonObject.toString();
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+
+    }
+
+}
---
+++ android/app/src/main/java/app/croma/HelpAnimator.java
@@ -0,0 +1,49 @@
+package app.croma;
+
+
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+
+public class HelpAnimator implements Animation.AnimationListener {
+    private Animation animation;
+    private View noColorHelp;
+    public HelpAnimator(View noColorHelp) {
+        this.noColorHelp = noColorHelp;
+    }
+
+    public void cancelGracefully() {
+        if (animation != null) {
+            animation.setDuration(150);
+       }
+    }
+    @Override
+    public void onAnimationStart(Animation animation) {
+        noColorHelp.bringToFront();
+        this.animation = animation;
+        noColorHelp.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                HelpAnimator.this.cancelGracefully();
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        noColorHelp.setOnTouchListener(null);
+        //ignore
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        try {
+            Thread.sleep(150);
+        } catch (InterruptedException e) {
+            //ignore
+            e.printStackTrace();
+        }
+    }
+}
