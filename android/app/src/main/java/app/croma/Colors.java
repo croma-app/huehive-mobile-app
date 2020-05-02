94a172ee453b0eeb5cd6eb44bf88e179a90dd21f
kamal <kkjoshi.it@gmail.com>
2020-05-02 18:45:18 +0530

    improved matching and basic tests

--- android/app/src/main/java/app/croma/Colors.java
+++ android/app/src/main/java/app/croma/Colors.java
@@ -3,19 +3,16 @@ package app.croma;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Colors {
-    private static String SEPARATOR = "\\s";
     private static Pattern COLOR_PATTERN = Pattern.compile("#[0-9a-fA-F]{6}");
     public static List<String> parse(String text) {
-        String words[] = text.split(SEPARATOR);
-        System.out.println("Words:" + Arrays.toString(words));
         List<String> colors = new ArrayList<>();
-        for (String word : words) {
-            if (COLOR_PATTERN.matcher(word).matches()) {
-                colors.add(word);
-            }
+        Matcher matcher = COLOR_PATTERN.matcher(text);
+        while (matcher.find()) {
+            colors.add(matcher.group());
         }
         return colors;
     }
