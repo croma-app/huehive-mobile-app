d741c01cbed6b0ed0991a6ea9aa8f603dfd58629
kamalkishor1991 <kkjoshi.it@gmail.com>
2020-05-13 18:16:20 +0000

    Deploying to gh-pages from  @ b874a0b9050d3e9e0dacd0a8e36f27721a4a912f 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c8a5c3a0b6cce91014c1c12566c5d77.js"
+  "/precache-manifest.0bf04de42c1155a6f863bcdd8a99f8ee.js"
 );
 
 workbox.skipWaiting();
