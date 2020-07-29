4f32eff17a4d453b371dafd00cc44a3485f3237a
Kamal Kishor Joshi <kkjoshi.it@gmail.com>
2020-07-30 02:07:45 +0530

    Pro version screen (#80)
    * pro version screen with text
    
    * added croma button on pro screen
    
    * Google Java Format
    
    * added pro version page
    
    * added croma module changes
    
    * Google Java Format
    
    * handle the already pro user case
    
    * few improvements
    
    * Google Java Format
    
    Co-authored-by: bhuwan <bhuwanchandra.it@gmail.com>
    Co-authored-by: github-actions <>

--- libs/Helpers.js
+++ libs/Helpers.js
@@ -1,9 +1,30 @@
-import { NativeModules, Platform } from "react-native";
+import { NativeModules, Platform, ToastAndroid } from "react-native";
+import InAppBilling from "react-native-billing";
 
 const logEvent = (eventName, value) => {
-  if (Platform.OS == "android") {
-    NativeModules.CromaModule.logEvent(eventName, "" + value);
+  if (Platform.OS === "android") {
+    NativeModules.CromaModule.logEvent(
+      eventName,
+      isObject(value) ? JSON.stringify(value) : `${value}`
+    );
   }
 };
 
-export { logEvent };
+function isObject(value) {
+  return value && typeof value === "object" && value.constructor === Object;
+}
+
+const purchase = async function(setPurchase, purchaseType = "croma_pro") {
+  try {
+    await InAppBilling.open();
+    const details = await InAppBilling.purchase(purchaseType);
+    ToastAndroid.show("Congrats, You are now a pro user!", ToastAndroid.LONG);
+    setPurchase(details);
+  } catch (err) {
+    ToastAndroid.show(`Purchase unsucceessful ${err}`, ToastAndroid.LONG);
+  } finally {
+    await InAppBilling.close();
+  }
+};
+
+export { logEvent, purchase };
