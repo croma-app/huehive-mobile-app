b9db91ba2be46d999a8132f20c494adbe36b30f0
bhuwan <bhuwanchandra.it@gmail.com>
2020-08-14 22:40:47 +0530

    export palettes functionality added

--- screens/ImportExportScreen.js
+++ screens/ImportExportScreen.js
@@ -1,16 +1,60 @@
-import React from "react";
-import { ScrollView, StyleSheet, Text } from "react-native";
+import React, { useContext } from "react";
+import {
+  ScrollView,
+  ToastAndroid,
+  StyleSheet,
+  PermissionsAndroid
+} from "react-native";
 import { View } from "react-native-animatable";
-import CromaButton from "../components/CromaButton";
 import { CromaContext } from "../store/store";
+import CromaButton from "../components/CromaButton";
 import { purchase, logEvent } from "../libs/Helpers";
+const longToast = function(msg) {
+  ToastAndroid.show(msg, ToastAndroid.LONG);
+};
+const exportPalettes = allPalettes => {
+  var RNFS = require("react-native-fs");
+
+  var path = RNFS.DownloadDirectoryPath + "/croma.palettes.json";
+  // write the file
+  allPalettes["version"] = "V1";
+  RNFS.writeFile(path, JSON.stringify(allPalettes), "utf8")
+    .then(success => {
+      longToast("Saved in Downloads...");
+    })
+    .catch(err => {
+      longToast(err.message);
+    });
+};
+
+saveFile = async allPalettes => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      exportPalettes(allPalettes);
+    } else {
+      longToast("Permission denied");
+    }
+  } catch (err) {
+    longToast(err);
+  }
+};
 
 export default function ImportExportScreen(props) {
+  const { allPalettes } = useContext(CromaContext);
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <View>
         <CromaButton>Import palettes from file</CromaButton>
-        <CromaButton>Export palettes as a file</CromaButton>
+        <CromaButton
+          onPress={() => {
+            saveFile(allPalettes);
+          }}
+        >
+          Export palettes as a file
+        </CromaButton>
       </View>
     </ScrollView>
   );
