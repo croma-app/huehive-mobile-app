c0fda2271ec3cf9f4010885f9b087216718cff46
kamal <kkjoshi.it@gmail.com>
2020-10-14 20:35:46 +0530

    styling

--- screens/PaletteLibraryScreen.js
+++ screens/PaletteLibraryScreen.js
@@ -3,6 +3,7 @@ import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { logEvent } from "../libs/Helpers";
 import Touchable from "react-native-platform-touchable";
 import { material } from "react-native-typography";
+import Colors from "../constants/Colors";
 const allPalettes = require("../constants/palettes/palettes").default;
 
 export default function PaletteLibraryScreen(props) {
@@ -13,6 +14,7 @@ export default function PaletteLibraryScreen(props) {
       {allPalettes.map(palettes => {
         return (
           <Touchable
+            style={styles.row}
             onPress={() => {
               logEvent("hm_matrial_palettes");
               props.navigation.navigate("CommonPalettes", { input: palettes });
@@ -41,6 +43,12 @@ const styles = StyleSheet.create({
   container: {
     paddingLeft: 12,
     paddingRight: 12
+  },
+  row: {
+    backgroundColor: Colors.white,
+    marginVertical: 8,
+    elevation: 1,
+    padding: 4
   },
   title: {
     ...material.title
