72dbebeaf243e0592160ed5f9232e620557f0a3a
Kamal Kishor Joshi <kkjoshi.it@gmail.com>
2020-10-19 22:48:09 +0530

    copy on colors list screen and tick mark in header to save a new palette (#107)
    * copy on colors list screen and wip for tick mark in header
    
    * add custom header for android with tick mark in the header

--- screens/ColorListScreen.js
+++ screens/ColorListScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { SingleColorView } from "../components/SingleColorView";
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View, Text, Platform } from "react-native";
 import CromaButton from "../components/CromaButton";
 import { logEvent } from "../libs/Helpers";
+import Touchable from "react-native-platform-touchable";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function ColorListScreen(props) {
   const colors = uniqueColors(props.navigation.getParam("colors"));
@@ -38,10 +40,51 @@ function uniqueColors(colors) {
   return uniqueColors;
 }
 
-ColorListScreen.navigationOptions = {
-  title: "Colors"
+const CustomHeader = props => {
+  const colors = uniqueColors(props.navigation.getParam("colors"));
+  const suggestedName = props.navigation.getParam("suggestedName");
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "95%"
+      }}
+    >
+      <Text
+        style={{
+          color: "#ffffff",
+          fontSize: 18
+        }}
+      >
+        Colors
+      </Text>
+      <>
+        <Touchable
+          onPress={() =>
+            props.navigation.navigate("SavePalette", {
+              colors: colors,
+              name: suggestedName
+            })
+          }
+        >
+          <MaterialIcons name="done" size={24} color="white" />
+        </Touchable>
+      </>
+    </View>
+  );
 };
-
+ColorListScreen.navigationOptions = ({ navigation }) => {
+  return {
+    headerTitle:
+      Platform.OS == "android" ? (
+        <CustomHeader navigation={navigation}></CustomHeader>
+      ) : (
+        "Colors"
+      )
+  };
+};
 const styles = StyleSheet.create({
   listview: {
     margin: 8
