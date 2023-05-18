bd42ceed05ac28ca7a4e5c32cdf9e91b4ced86a3
Kamal Kishor Joshi <kkjoshi.it@gmail.com>
2023-05-18 09:16:52 +0530

    Hsv color picker (#190)
    * poc implementation by using slider using react-native-slider-color-picker
    
    * HSB color picker support

---
+++ components/SliderColorPicker.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import {
+  SliderHuePicker,
+  SliderSaturationPicker,
+  SliderValuePicker
+} from 'react-native-slider-color-picker';
+import { Dimensions, StyleSheet, View, Text, TextInput } from 'react-native';
+
+import tinycolor from 'tinycolor2'; // TODO: change tinycolor to pigment
+import { logEvent } from '../libs/Helpers';
+
+const { width } = Dimensions.get('window');
+
+export default function SliderColorPicker(props) {
+  const [color, setColor] = React.useState(props.color);
+  const hsv = tinycolor(color).toHsv();
+  logEvent('slider_color_picker_component');
+  const changeColor = (colorHsvOrRgb, resType) => {
+    if (resType === 'end') {
+      setColor(tinycolor(colorHsvOrRgb).toHexString());
+      if (props.setColor) {
+        // console.log('colorHsvOrRgb', tinycolor(colorHsvOrRgb).toHexString());
+        props.setColor(tinycolor(colorHsvOrRgb).toHexString());
+      }
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          marginHorizontal: 24,
+          marginTop: 20,
+          height: 36,
+          width: width - 48
+        }}>
+        <Text>Hue({hsv.h.toFixed(1)}) </Text>
+        <SliderHuePicker
+          oldColor={color}
+          trackStyle={[{ height: 12, width: width - 48 }]}
+          thumbStyle={styles.thumb}
+          useNativeDriver={true}
+          onColorChange={changeColor}
+        />
+      </View>
+      <View style={{ marginHorizontal: 24, marginTop: 20, height: 36, width: width - 48 }}>
+        <Text>Seturation({hsv.s.toFixed(2)})</Text>
+        <SliderSaturationPicker
+          oldColor={color}
+          trackStyle={[{ height: 12, width: width - 48 }]}
+          thumbStyle={styles.thumb}
+          useNativeDriver={true}
+          onColorChange={changeColor}
+          style={{
+            height: 12,
+            borderRadius: 6,
+            backgroundColor: tinycolor({
+              h: tinycolor(color).toHsv().h,
+              s: 1,
+              v: 1
+            }).toHexString()
+          }}
+        />
+      </View>
+      <View style={{ marginHorizontal: 24, marginTop: 20, height: 36, width: width - 48 }}>
+        <Text>Brightness({hsv.v.toFixed(2)})</Text>
+        <SliderValuePicker
+          oldColor={color}
+          minimumValue={0.02}
+          step={0.05}
+          trackStyle={[{ height: 12, width: width - 48 }]}
+          trackImage={require('react-native-slider-color-picker/brightness_mask.png')}
+          thumbStyle={styles.thumb}
+          onColorChange={changeColor}
+          style={{ height: 12, borderRadius: 6, backgroundColor: 'black' }}
+        />
+      </View>
+      <View style={styles.selectedColorView}>
+        <TextInput style={styles.input} value={color} onChangeText={(color) => setColor(color)} />
+        <View style={[styles.selectedColor, { backgroundColor: color }]}></View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  thumb: {
+    width: 20,
+    height: 20,
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowRadius: 2,
+    shadowOpacity: 0.35
+  },
+  input: {
+    width: '50%',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
+  },
+  selectedColorView: {
+    marginTop: 10,
+    flexDirection: 'row',
+    flex: 2,
+    padding: 10
+  },
+  selectedColor: {
+    width: '50%'
+  }
+});
--- screens/ColorPickerScreen.js
+++ screens/ColorPickerScreen.js
@@ -1,29 +1,37 @@
 import React, { useState, useContext } from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
+import { ScrollView, View, StyleSheet, Text } from 'react-native';
 import CromaButton from '../components/CromaButton';
 import { CromaColorPicker as ColorPicker } from 'croma-color-picker';
 import { logEvent } from '../libs/Helpers';
 import { CromaContext } from '../store/store';
+import SliderColorPicker from '../components/SliderColorPicker';
+import ColorPickerDropdown from '../components/ColorPickerDropdown';
+
 export default function ColorPickerScreen({ navigation }) {
   const [color, setColor] = useState('#db0a5b');
+  const [colorPicker, setColorPicker] = useState('basic'); // ['basic', 'HSB']
   const { colorPickerCallback } = useContext(CromaContext);
 
   logEvent('color_picker_screen');
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
+      <ColorPickerDropdown onChange={(value) => setColorPicker(value)} />
       <View style={styles.container}>
-        <ColorPicker
-          onChangeColor={(color) => {
-            setColor(color);
-          }}
-          style={[{ height: 350 }]}
-        />
+        {colorPicker === 'HSB' ? (
+          <SliderColorPicker color={`${color}`} setColor={setColor} />
+        ) : (
+          <ColorPicker
+            onChangeColor={(color) => {
+              setColor(color);
+            }}
+            style={[{ height: 350 }]}
+          />
+        )}
         <CromaButton
           onPress={() => {
             navigation.goBack();
             colorPickerCallback({ color });
-          }}
-        >
+          }}>
           Done
         </CromaButton>
       </View>
