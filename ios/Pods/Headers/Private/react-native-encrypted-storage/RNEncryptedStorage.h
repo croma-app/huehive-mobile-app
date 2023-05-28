5d8528302980bc780bad61d3d8da2d92affc33ce
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-05-28 22:41:18 +0500

    moved the logic in to a function

---
+++ ios/Pods/Headers/Private/react-native-encrypted-storage/RNEncryptedStorage.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-encrypted-storage/ios/RNEncryptedStorage.h
---
+++ ios/Pods/Headers/Public/react-native-config/RNCConfigModule.h
@@ -0,0 +1,1 @@
+../../../../../node_modules/react-native-config/ios/ReactNativeConfig/RNCConfigModule.h
