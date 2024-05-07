71384c360c9bbbee9602a3c029028efc66920f4f
bhuwan joshi <bhuwanchandra.it@gmail.com>
2024-05-07 09:40:53 +0530

    explore palette ui added

--- screens/PaletteLibraryScreen.js
+++ screens/PaletteLibraryScreen.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ScrollView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { logEvent } from '../libs/Helpers';
 import { material } from 'react-native-typography';
 import Colors from '../constants/Colors';
+import network from '../network';
 import useApplicationStore from '../hooks/useApplicationStore';
+import { PalettePreviewCard } from '../components/PalettePreviewCard';
 const allPalettes = require('../constants/palettes/palettes').default;
 
 export default function PaletteLibraryScreen({ navigation }) {
-  logEvent('palette_library_screen');
-
+  const [explorePalettes, setExplorePalettes] = useState();
   const { setCommonPalettes } = useApplicationStore();
+  useEffect(() => {
+    logEvent('palette_library_screen');
+  }, []);
+
+  useEffect(() => {
+    const getExplorePalette = async () => {
+      const res = await network.getExplorePalettes();
+      const palettes = res.data.search_results.map((p) => {
+        return {
+          name: p.user_query,
+          colors: p.colors.map((color) => ({
+            id: color.id,
+            name: color.name,
+            color: color.hex
+          }))
+        };
+      });
+      setExplorePalettes(palettes);
+    };
+    getExplorePalette();
+  }, [explorePalettes]);
+  console.log({ explorePalettes });
 
   return (
     <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      {explorePalettes?.map((palette, index) => (
+        <PalettePreviewCard
+          key={index}
+          onPress={() => {
+            navigation.navigate('ColorList', {
+              colors: palette.colors,
+              suggestedName: name + ' - ' + palette.name
+            });
+          }}
+          colors={palette.colors}
+          name={palette.name}
+        />
+      ))}
       {allPalettes.map((palettes, index) => {
         return (
           <TouchableOpacity
@@ -48,7 +84,8 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     elevation: 1,
     padding: 8,
-    borderRadius: 8
+    borderRadius: 8,
+    marginBottom: 10
   },
   title: {
     ...material.title
