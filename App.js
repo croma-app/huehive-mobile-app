5d653d9762a442011d77cec2b5bfc603c64afa6c
bhuwan joshi <bhuwanchandra.it@gmail.com>
2024-05-11 10:32:37 +0530

    Removed onboarding screen

--- App.js
+++ App.js
@@ -1,32 +1,16 @@
 import React, { useEffect } from 'react';
-import { ActivityIndicator, StatusBar } from 'react-native';
-import AuthOnboardingScreen from './components/AuthOnboardingScreen';
+import { StatusBar } from 'react-native';
 import ApplicationRoot from './ApplicationRoot';
-import Storage from './libs/Storage';
 import { withIAPContext } from 'react-native-iap';
 import useUserData from './hooks/useUserData';
 import Colors from './constants/Colors';
 
 const App = () => {
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [isLoginOverlayStepDone, setIsLoginOverlayStepDone] = React.useState(true);
   const { loadUserData } = useUserData();
-  React.useEffect(() => {
-    Storage.isLoginOverlayStepDone().then((value) => {
-      if (value === 'yes') {
-        setIsLoginOverlayStepDone(false);
-      }
-      setIsLoading(true);
-    });
-  }, []);
 
   useEffect(() => {
     loadUserData();
   }, [loadUserData]);
-
-  const markLoginStepDone = () => {
-    setIsLoginOverlayStepDone(false);
-  };
 
   return (
     <>
@@ -37,15 +21,7 @@ const App = () => {
         translucent={false}
         networkActivityIndicatorVisible={true}
       />
-      {isLoading ? (
-        isLoginOverlayStepDone ? (
-          <AuthOnboardingScreen markLoginStepDone={markLoginStepDone} />
-        ) : (
-          <ApplicationRoot />
-        )
-      ) : (
-        <ActivityIndicator />
-      )}
+      <ApplicationRoot />
     </>
   );
 };
