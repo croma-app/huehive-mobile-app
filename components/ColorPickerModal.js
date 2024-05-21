5e2b1d4465e5415100ff3866d57e7c7d115242ec
bhuwan joshi <bhuwanchandra.it@gmail.com>
2024-05-21 23:05:09 +0530

    Fixed status bar color

--- components/ColorPickerModal.js
+++ components/ColorPickerModal.js
@@ -149,7 +149,7 @@ const styles = StyleSheet.create({
   },
   activeTab: {
     borderBottomWidth: 2,
-    borderBottomColor: Colors.brandColor
+    borderBottomColor: Colors.primary
   },
   tabText: {
     fontSize: 12,
@@ -194,7 +194,7 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   doneButton: {
-    backgroundColor: Colors.brandColor,
+    backgroundColor: Colors.primary,
     borderRadius: 20,
     width: 40,
     height: 40,
