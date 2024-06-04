e3d7678a26cee002839f230e67c4d04aba26d13a
Kamal Joshi <kkjoshi.it@gmail.com>
2024-06-04 19:24:35 +0530

    fix deep linking when app is already open

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Color from 'pigment/full';
 import {
   ActivityIndicator,
@@ -7,7 +7,8 @@ import {
   ScrollView,
   StyleSheet,
   View,
-  Text
+  Text,
+  Alert
 } from 'react-native';
 import { PaletteCard } from '../components/PaletteCard';
 import GridActionButton from '../components/GridActionButton';
@@ -23,11 +24,12 @@ import useApplicationStore from '../hooks/useApplicationStore';
 const HomeScreen = function ({ navigation, route }) {
   const { isLoading, allPalettes, isPro } = useApplicationStore();
   const [pickImageLoading, setPickImageLoading] = useState(false);
+
   const getPermissionAsync = async () => {
-    if (Platform?.OS === 'ios') {
-      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (Platform.OS === 'ios') {
+      const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
       if (status !== 'granted') {
-        alert('Sorry, we need camera roll permissions to make this work!');
+        Alert.alert('Sorry, we need camera roll permissions to make this work!');
       }
     }
   };
@@ -36,58 +38,79 @@ const HomeScreen = function ({ navigation, route }) {
     logEvent('home_screen');
   }, []);
 
+  const handleDeepLink = useCallback(
+    (url) => {
+      try {
+        const urlParts = url.split('?');
+        const path = urlParts[0].split('/');
+        const colorsPart = path[path.length - 1];
+
+        // Split the colors part by `-` to get the color codes
+        const colors = colorsPart.split('-');
+
+        // Parse the query parameters
+        const queryParamsString = urlParts[1] || '';
+        const queryParams = {};
+        queryParamsString.split('&').forEach((param) => {
+          const [key, value] = param.split('=');
+          queryParams[key] = decodeURIComponent(value);
+        });
+
+        const suggestedName = queryParams['name'];
+        navigation.navigate('SavePalette', {
+          colors: colors.map((color) => {
+            return { color: color.color };
+          }),
+          suggestedName: suggestedName
+        });
+      } catch (error) {
+        notifyMessage('Error parsing url: ' + error.message);
+        navigation.navigate('Home');
+      }
+    },
+    [navigation]
+  );
+
   useEffect(() => {
-    if (Platform?.OS === 'android') {
-      // Deep linking code
-      // https://medium.com/react-native-training/deep-linking-your-react-native-app-d87c39a1ad5e
-      // https://huehive.co/color_palettes/f8a6a1-dcdcdc-f2b3ad-e0e0e0-aec6cf-c4c4c4?name=hello
+    if (Platform.OS === 'android' || Platform.OS === 'ios') {
+      // Handle deep link when the app is launched
       Linking.getInitialURL().then((url) => {
         if (url) {
           logEvent('deep_linking_open_link');
-
-          try {
-            // Manually parse the URL
-            const urlParts = url.split('?');
-            const path = urlParts[0].split('/');
-            const colorsPart = path[path.length - 1];
-
-            // Split the colors part by `-` to get the color codes
-            const colors = colorsPart.split('-');
-
-            // Parse the query parameters
-            const queryParamsString = urlParts[1] || '';
-            const queryParams = {};
-            queryParamsString.split('&').forEach((param) => {
-              const [key, value] = param.split('=');
-              queryParams[key] = decodeURIComponent(value);
-            });
-
-            const suggestedName = queryParams['name'];
-
-            // Navigate to the SavePalette screen with the extracted colors and name
-            navigation.navigate('SavePalette', {
-              colors: [...new Set(colors)], // Ensuring unique colors
-              suggestedName: suggestedName
-            });
-          } catch (error) {
-            notifyMessage('Error parsing url: ' + error.message);
-            navigation.navigate('Home');
-          }
+          handleDeepLink(url);
         }
       });
 
+      // Handle deep link when the app is already running
+      const linkingListener = Linking.addEventListener('url', (event) => {
+        if (event.url) {
+          logEvent('deep_linking_open_link');
+          handleDeepLink(event.url);
+        }
+      });
+
+      // Cleanup listener on unmount
+      return () => {
+        linkingListener.remove();
+      };
+    }
+
+    getPermissionAsync();
+  }, [handleDeepLink, navigation]);
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
       ShareMenu.getSharedText((text) => {
         if (text && typeof text === 'string') {
           const colors = Color.parse(text);
           logEvent('get_shared_text', { length: colors.length });
-          for (var i = 0, l = colors.length; i < l; i++) {
+          for (let i = 0, l = colors.length; i < l; i++) {
             colors[i] = { color: colors[i].tohex().toLowerCase() };
           }
           navigation.navigate('SavePalette', { colors });
         }
       });
     }
-    getPermissionAsync();
   }, [navigation]);
 
   if (isLoading) {
@@ -95,7 +118,7 @@ const HomeScreen = function ({ navigation, route }) {
       <View style={styles.loadingContainer}>
         <View>
           <ActivityIndicator size="large" color={Colors.primary} />
-          <Text style={styles.loadingText}>Retriving your stunning color palettes...</Text>
+          <Text style={styles.loadingText}>Retrieving your stunning color palettes...</Text>
         </View>
       </View>
     );
@@ -104,7 +127,7 @@ const HomeScreen = function ({ navigation, route }) {
       <View style={styles.container}>
         {pickImageLoading ? <ActivityIndicator /> : <View />}
         <ScrollView showsVerticalScrollIndicator={false}>
-          {allPalettes.length == 0 && (
+          {allPalettes.length === 0 && (
             <Text style={styles.noColorPaletteMessage}>
               No color palettes found. Tap the + button below to create a new one.
             </Text>
