0e519e6e224194fc6cba00249760be9d4878b556
Kamal Joshi <kkjoshi.it@gmail.com>
2024-07-28 11:23:14 +0530

    add react-native-exception-handler for generic error handling

--- index.js
+++ index.js
@@ -6,5 +6,38 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import './i18n';
+import { setJSExceptionHandler, setNativeExceptionHandler } from 'react-native-exception-handler';
+import { notifyMessage, sendClientError } from './libs/Helpers';
 
+// Custom JS error handler function
+const jsErrorHandler = (error, isFatal) => {
+  // Handle the error, e.g., log it, show an alert, or send it to an analytics server
+  console.log(`JS Error: ${isFatal ? 'Fatal' : 'Non-fatal'}: ${error}`);
+  if (isFatal) {
+    // Show an alert for fatal errors
+    notifyMessage(
+      'Unexpected error occurred' +
+        `Error: ${error.name} ${error.message}\nWe have reported this to our team ! Please close the app and start again!`,
+      2000
+    );
+    sendClientError('react-native-generic-js-error', 'Fatal - ' + isFatal + ' ' + error.message);
+  } else {
+    sendClientError('react-native-generic-js-error', 'Fatal - ' + isFatal + ' ' + error.message);
+    console.log(error); // Log non-fatal errors to the console
+  }
+};
+
+// Set the global JS error handler
+setJSExceptionHandler(jsErrorHandler, true);
+
+// Custom native error handler function (Android only)
+const nativeErrorHandler = (exceptionString) => {
+  console.log(`Native Error: ${exceptionString}`);
+  notifyMessage('react-native-generic-native-error', exceptionString);
+};
+
+// Set the global native error handler
+setNativeExceptionHandler(nativeErrorHandler);
+
+// Register the main app component
 AppRegistry.registerComponent(appName, () => App);
