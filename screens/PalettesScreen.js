21575e5626fc7aed528d4734053179349237bbff
Kamal Joshi <kkjoshi.it@gmail.com>
2024-08-24 10:42:12 +0530

    improve UI little more

--- screens/PalettesScreen.js
+++ screens/PalettesScreen.js
@@ -7,8 +7,55 @@ import { PalettePreviewCard } from '../components/PalettePreviewCard';
 import { logEvent } from '../libs/Helpers';
 import useApplicationStore from '../hooks/useApplicationStore';
 import { useNavigation } from '@react-navigation/native';
+import Colors from '../constants/Styles';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 function PalettesScreen() {
+  const { detailedColor } = useApplicationStore();
+  const navigation = useNavigation();
+
+  const parseCamelCase = (text) => {
+    if (typeof text !== 'string') {
+      return '';
+    }
+    return text
+      .replace(/([a-z])([A-Z])/g, '$1 $2')
+      .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+      .replace(/^./, (str) => str.toUpperCase());
+  };
+
+  const fullColor = new Color(detailedColor);
+  let items = [];
+  for (const i in fullColor) {
+    if (/.*scheme$/i.test(i) && typeof fullColor[i] === 'function') {
+      let colors = [];
+      const paletteColors = fullColor[i]();
+      paletteColors.forEach((c) => colors.push({ color: c.tohex() }));
+      items.push(
+        <PalettePreviewCard
+          onPress={() => {
+            navigation.navigate('ColorList', { colors: colors });
+          }}
+          key={i.toString()}
+          colors={colors}
+          name={parseCamelCase(i.toString())}
+        />
+      );
+    }
+  }
+
+  useEffect(() => {
+    logEvent('palettes_screen');
+  }, []);
+
+  return (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      {items}
+    </ScrollView>
+  );
+}
+
+function PalettesScreenAI() {
   const { detailedColor } = useApplicationStore();
   const navigation = useNavigation();
 
@@ -74,48 +121,55 @@ export default function PalettesTabNavigator() {
       screenOptions={({ route }) => ({
         headerShown: false,
         tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
           if (route.name === 'Color Theory') {
-            iconName = 'color-palette';
+            return (
+              <Icon
+                name="color-palette"
+                size={size}
+                color={focused ? Colors.primary : color} // Highlight the selected tab
+                style={{
+                  shadowColor: focused ? Colors.primary : '#000',
+                  shadowOpacity: 0.2,
+                  shadowRadius: 10
+                }}
+              />
+            );
           } else if (route.name === 'AI') {
-            iconName = 'construct';
+            return (
+              <FontAwesome
+                name="magic"
+                size={size}
+                color={focused ? Colors.primary : color} // Highlight the selected tab
+                style={{
+                  shadowColor: focused ? Colors.primary : '#000',
+                  shadowOpacity: 0.2,
+                  shadowRadius: 10
+                }}
+              />
+            );
           }
-
-          return (
-            <Icon
-              name={iconName}
-              size={size}
-              color={focused ? '#007AFF' : color} // Highlight the selected tab
-              style={{
-                shadowColor: focused ? '#007AFF' : '#000',
-                shadowOpacity: 0.2,
-                shadowRadius: 10
-              }}
-            />
-          );
         },
         tabBarStyle: {
-          backgroundColor: '#ffffff', // Bright background color
-          borderTopWidth: 0, // Remove the top border for a seamless look
-          elevation: 10, // Add elevation for Android shadow
-          shadowColor: '#000', // Shadow color
-          shadowOffset: { width: 0, height: 5 }, // Shadow position
-          shadowOpacity: 0.1, // Shadow transparency
-          shadowRadius: 10, // Shadow blur
-          paddingBottom: 10, // Extra padding at the bottom for a more balanced look
-          paddingTop: 10, // Extra padding at the top
-          height: 60 // Increase height for better touch targets
+          backgroundColor: '#ffffff',
+          borderTopWidth: 0,
+          elevation: 10,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 5 },
+          shadowOpacity: 0.1,
+          shadowRadius: 10,
+          paddingBottom: 10,
+          paddingTop: 10,
+          height: 60
         },
-        tabBarActiveTintColor: '#007AFF', // Active tab color (blue)
-        tabBarInactiveTintColor: '#8e8e93', // Inactive tab color (gray)
+        tabBarActiveTintColor: Colors.primary,
+        tabBarInactiveTintColor: Colors.gray,
         tabBarLabelStyle: {
           fontSize: 12, // Slightly larger font size for better readability
           fontWeight: '600' // Bold text for the labels
         }
       })}>
       <Tab.Screen name="Color Theory" component={PalettesScreen} />
-      <Tab.Screen name="AI" component={PalettesScreen} />
+      <Tab.Screen name="AI" component={PalettesScreenAI} />
     </Tab.Navigator>
   );
 }
