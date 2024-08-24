164ea1c231423c54b450dad1d3c86051ae4fcd5a
Kamal Kishor Joshi <kkjoshi.it@gmail.com>
2024-08-24 22:02:12 +0530

    Color palettes generate with AI  (#248)
    * basic UX with bottom tabs
    
    * improve UI little more
    
    * integration with backend

--- screens/PalettesScreen.js
+++ screens/PalettesScreen.js
@@ -1,17 +1,20 @@
-import React, { useEffect, useLayoutEffect } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import { ScrollView, StyleSheet, ActivityIndicator } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Color from 'pigment/full';
 import { PalettePreviewCard } from '../components/PalettePreviewCard';
 import { logEvent } from '../libs/Helpers';
 import useApplicationStore from '../hooks/useApplicationStore';
-export default function PalettesScreen({ navigation }) {
+import { useNavigation } from '@react-navigation/native';
+import Colors from '../constants/Styles';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { generateUsingColor } from '../network/color_palette';
+
+function PalettesScreen() {
   const { detailedColor } = useApplicationStore();
+  const navigation = useNavigation();
 
-  useLayoutEffect(() => {
-    navigation.setOptions({ title: detailedColor });
-  }, [detailedColor]);
-
-  // Convert camelCase to sentence
   const parseCamelCase = (text) => {
     if (typeof text !== 'string') {
       return '';
@@ -21,6 +24,7 @@ export default function PalettesScreen({ navigation }) {
       .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
       .replace(/^./, (str) => str.toUpperCase());
   };
+
   const fullColor = new Color(detailedColor);
   let items = [];
   for (const i in fullColor) {
@@ -52,9 +56,122 @@ export default function PalettesScreen({ navigation }) {
   );
 }
 
+function PalettesScreenAI() {
+  const { detailedColor } = useApplicationStore();
+  const [loading, setLoading] = useState(true);
+  const [palettes, setPalettes] = useState([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const fetchPalettes = async () => {
+      try {
+        const response = await generateUsingColor(detailedColor);
+        const generatedPalettes = response.data.palettes;
+        setPalettes(generatedPalettes);
+      } catch (error) {
+        console.error('Error fetching AI palettes:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPalettes();
+    logEvent('palettes_screen_ai');
+  }, [detailedColor]);
+
+  return (
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        palettes.map((palette, index) => {
+          const colors = palette.colors.map((color) => ({ color: color.hex }));
+          return (
+            <PalettePreviewCard
+              onPress={() => {
+                navigation.navigate('ColorList', { suggestedName: palette.name, colors });
+              }}
+              key={index.toString()}
+              colors={colors}
+              name={palette.name}
+            />
+          );
+        })
+      )}
+    </ScrollView>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     paddingLeft: 12,
     paddingRight: 12
   }
 });
+
+const Tab = createBottomTabNavigator();
+
+export default function PalettesTabNavigator() {
+  const { detailedColor } = useApplicationStore();
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({ title: detailedColor });
+  }, [detailedColor, navigation]);
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          if (route.name === 'Color Theory') {
+            return (
+              <Icon
+                name="color-palette"
+                size={size}
+                color={focused ? Colors.primary : color} // Highlight the selected tab
+                style={{
+                  shadowColor: focused ? Colors.primary : '#000',
+                  shadowOpacity: 0.2,
+                  shadowRadius: 10
+                }}
+              />
+            );
+          } else if (route.name === 'AI') {
+            return (
+              <FontAwesome
+                name="magic"
+                size={size}
+                color={focused ? Colors.primary : color} // Highlight the selected tab
+                style={{
+                  shadowColor: focused ? Colors.primary : '#000',
+                  shadowOpacity: 0.2,
+                  shadowRadius: 10
+                }}
+              />
+            );
+          }
+        },
+        tabBarStyle: {
+          backgroundColor: '#ffffff',
+          borderTopWidth: 0,
+          elevation: 10,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 5 },
+          shadowOpacity: 0.1,
+          shadowRadius: 10,
+          paddingBottom: 10,
+          paddingTop: 10,
+          height: 60
+        },
+        tabBarActiveTintColor: Colors.primary,
+        tabBarInactiveTintColor: Colors.gray,
+        tabBarLabelStyle: {
+          fontSize: 12, // Slightly larger font size for better readability
+          fontWeight: '600' // Bold text for the labels
+        }
+      })}>
+      <Tab.Screen name="Color Theory" component={PalettesScreen} />
+      <Tab.Screen name="AI" component={PalettesScreenAI} />
+    </Tab.Navigator>
+  );
+}
